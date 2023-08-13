ddf51ae7d0917bf94eba6ec42be386b7524c265e
RitchielRez <52417892+ritchielrez@users.noreply.github.com>
2023-08-13 15:09:48 +0600

    refactor: some refactoring

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ def swap_elems(in_list: list, i, j):
     in_list[j] = tmp
 
 def quicksort(in_list: list, low: int, high: int) -> list:
-    pivot_index = high - 1
+    pivot_index = high
     pivot = in_list[pivot_index]
     i = low - 1
     j = low
 
     print(in_list)
 
-    if low >= pivot_index:
+    if low < 0 or low >= pivot_index:
         return None
 
     while j < pivot_index:
@@ -22,7 +22,7 @@ def quicksort(in_list: list, low: int, high: int) -> list:
 
     swap_elems(in_list, pivot_index, i + 1)
 
-    quicksort(in_list, low, i + 1)
+    quicksort(in_list, low, i)
     quicksort(in_list, i + 2, high)
 
     return in_list
@@ -31,6 +31,6 @@ if __name__ == "__main__":
     # list_of_ints : list[int] = [10, 80, 30, 90, 40]
     # After 1 stage of sorting, [10, 30, 40, 90, 80]
     # Here 40 is the pivot
-    list_of_ints: list[int] = [23, 12, 3, 6, 8, 9]
+    list_of_ints: list[int] = [14, 68, 96, 99, 67, 95, 33, 58, 1, 50, 77, 72, 31, 52, 48, 47, 30, 85, 18, 100]
     print("Before sorting, list =", list_of_ints)
-    print("After sorting, list =", quicksort(list_of_ints, 0, len(list_of_ints)))
+    print("After sorting, list =", quicksort(list_of_ints, 0, len(list_of_ints) - 1))
